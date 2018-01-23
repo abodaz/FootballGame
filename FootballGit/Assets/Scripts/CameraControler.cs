ddecb5acc0304940dd44833c4e37c470a4b582fd
Abdallah Al-Azzami <abodaz100@gmail.com>
2018-01-24 00:36:59 +0200

    Abod 24/1
    UI ,new Ball ,new Goal Gate

---
+++ FootballGit/Assets/Scripts/CameraControler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour {
+
+    public GameObject ballPref;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - ballPref.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = ballPref.transform.position + offset;
+	}
+}
